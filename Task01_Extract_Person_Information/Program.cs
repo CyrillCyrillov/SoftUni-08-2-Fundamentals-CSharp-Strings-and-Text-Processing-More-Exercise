ae75972c7842adda104c6fe7c74f6857213f1804
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-20 21:47:37 +0200

    Add files via upload

---
+++ Task01_Extract_Person_Information/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task01_Extract_Person_Information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                string data = Console.ReadLine();
+                string name = GetInfo(data, '@', '|');
+                string age = GetInfo(data, '#', '*');
+
+                Console.WriteLine($"{name} is {age} years old.");
+
+            }
+
+        }
+
+        private static string GetInfo(string data, char start, char end)
+        {
+            int startIndex = data.IndexOf(start.ToString());
+            int endIndex = data.LastIndexOf(end.ToString());
+            string result = string.Empty;
+
+            for (int i = startIndex + 1; i < endIndex; i++)
+            {
+                result += data[i];
+            }
+            
+            return result;
+        }
+    }
+}
